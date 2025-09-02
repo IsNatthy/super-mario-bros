ed31d4b1200cf1de789051ecf78933bf532ed63a
Nathalia Avila <nathyavila05@gmail.com>
2025-09-01 21:35:36 -0500

    Add assets and basic game setup
    Basic game setup includes the animation for mario in different states (jump, walk, idle), and a basic collision and physics config.

--- game.js
+++ game.js
@@ -1,1 +1,107 @@
-console.log("Hello World")
+/* global Phaser */
+
+const config = {
+    type: Phaser.AUTO, // webgl, canvas
+    width: 256,
+    height: 244,
+    backgroundColor: '#049cd8',
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload,
+        create,
+        update
+    }
+}
+
+new Phaser.Game(config)
+
+function preload() {
+    this.load.image(
+        'cloud1', // id
+        'assets/scenery/overworld/cloud1.png'
+    )
+
+    this.load.image(
+        'floorbricks',
+        'assets/scenery/overworld/floorbricks.png'
+    )
+
+    this.load.spritesheet(
+        'mario', // id
+        'assets/entities/mario.png',
+        { frameWidth: 18, frameHeight: 16 }
+    )
+}
+
+function create() {
+    this.add.image(100, 50, 'cloud1')
+        .setOrigin(0, 0)
+        .setScale(0.15)
+
+    this.floor = this.physics.add.staticGroup()
+
+    this.floor
+        .create(0, config.height - 16, 'floorbricks')
+        .setOrigin(0, 0.5)
+        .refreshBody()
+        
+    this.floor
+        .create(150, config.height - 16, 'floorbricks')
+        .setOrigin(0, 0.5)
+        .refreshBody()
+
+    this.mario = this.physics.add.sprite(50, 100, 'mario')
+        .setOrigin(0, 1)
+        .setCollideWorldBounds(true)
+        .setGravityY(500)
+
+    this.physics.add.collider(this.mario, this.floor)
+
+    this.anims.create({
+        key: 'mario-walk',
+        frames: this.anims.generateFrameNumbers(
+            'mario',
+            { start: 1, end: 3 }
+        ),
+        frameRate: 12,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: 'mario-idle',
+        frames: [{ key: 'mario', frame: 0 }]
+    })
+
+    this.anims.create({
+        key: 'mario-jump',
+        frames: [{ key: 'mario', frame: 5 }]
+    })
+
+    this.keys = this.input.keyboard.createCursorKeys()
+}
+
+function update() {
+    if (this.keys.left.isDown) {
+        this.mario.x -= 2
+        this.mario.anims.play('mario-walk', true) // true = ignore if already playing
+        this.mario.flipX = true
+    } else if (this.keys.right.isDown) {
+        this.mario.x += 2
+        this.mario.flipX = false
+        this.mario.anims.play('mario-walk', true)
+    } else {
+        this.mario.anims.play('mario-idle', true)
+    }
+
+    if (this.keys.up.isDown && this.mario.body.touching.down) {
+        this.mario.setVelocityY(-300) 
+        this.mario.anims.play('mario-jump', true)
+    }
+}
