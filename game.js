fb5bfb007fd96680631af720e9a8630f5e7b799a
Nathalia Avila <nathyavila05@gmail.com>
2025-09-10 19:58:11 -0500

    Refactor Mario animations and add game over logic and sound

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 /* global Phaser */
 
+import { createAnimations } from "./animations.js"
+
 const config = {
     type: Phaser.AUTO, // webgl, canvas
     width: 256,
@@ -38,6 +40,8 @@ function preload() {
         'assets/entities/mario.png',
         { frameWidth: 18, frameHeight: 16 }
     )
+
+    this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
 }
 
 function create() {
@@ -60,42 +64,29 @@ function create() {
     this.mario = this.physics.add.sprite(50, 100, 'mario')
         .setOrigin(0, 1)
         .setCollideWorldBounds(true)
-        .setGravityY(500)
-
+        .setGravityY(300)
+        
+    this.physics.world.setBounds(0, 0, 2000, config.height)
     this.physics.add.collider(this.mario, this.floor)
 
-    this.anims.create({
-        key: 'mario-walk',
-        frames: this.anims.generateFrameNumbers(
-            'mario',
-            { start: 1, end: 3 }
-        ),
-        frameRate: 12,
-        repeat: -1
-    })
+    this.cameras.main.setBounds(0, 0, 2000, config.height)
+    this.cameras.main.startFollow(this.mario)
 
-    this.anims.create({
-        key: 'mario-idle',
-        frames: [{ key: 'mario', frame: 0 }]
-    })
-
-    this.anims.create({
-        key: 'mario-jump',
-        frames: [{ key: 'mario', frame: 5 }]
-    })
-
+    createAnimations(this)
     this.keys = this.input.keyboard.createCursorKeys()
 }
 
 function update() {
+    if (this.mario.isDead) return
+
     if (this.keys.left.isDown) {
-        this.mario.x -= 2
         this.mario.anims.play('mario-walk', true) // true = ignore if already playing
+        this.mario.x -= 2
         this.mario.flipX = true
     } else if (this.keys.right.isDown) {
+        this.mario.anims.play('mario-walk', true)
         this.mario.x += 2
         this.mario.flipX = false
-        this.mario.anims.play('mario-walk', true)
     } else {
         this.mario.anims.play('mario-idle', true)
     }
@@ -103,5 +94,20 @@ function update() {
     if (this.keys.up.isDown && this.mario.body.touching.down) {
         this.mario.setVelocityY(-300) 
         this.mario.anims.play('mario-jump', true)
+    }
+
+    if (this.mario.y >= config.height) {
+        this.mario.isDead = true
+        this.mario.anims.play('mario-dead')
+        this.mario.setCollideWorldBounds(false)
+        this.sound.add('gameover', { volume: 0.2 }).play()
+
+        setTimeout(() => {
+            this.mario.setVelocityY(-350)
+        }, 100)
+
+        setTimeout(() => {
+            this.scene.restart()
+        }, 2000)
     }
 }
